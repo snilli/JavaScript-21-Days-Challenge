55215f11c3e5eac2f0544ba72029778e64334ef4
snilli <mklx1319@gmail.com>
2021-09-19 21:08:14 +0700

    add duck hunt

--- src/06 - Duck Hunt/start.js
+++ src/06 - Duck Hunt/start.js
@@ -1,4 +1,20 @@
 "use strict";
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -8,6 +24,17 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
+var __values = (this && this.__values) || function(o) {
+    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+    if (m) return m.call(o);
+    if (o && typeof o.length === "number") return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+};
 var random = function (max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -16,12 +43,32 @@ var mapDuctToElement = function (duck) {
     duckElement.className = 'duck';
     duckElement.style.left = duck.x + "px";
     duckElement.style.top = duck.y + "px";
-    duckElement.style.background = "url(./left-1.png)";
     document.body.appendChild(duckElement);
     return duckElement;
 };
+var getDuckBackgroundImage = function (duckElement, duck) {
+    var direction = duck.speedX > 0 ? 'right' : 'left';
+    var animetion = duckElement.style.backgroundImage.includes('1') ? '2' : '1';
+    return "url(./" + direction + "-" + animetion + ".png)";
+};
+var moveDuck = function (duckElement, duck) {
+    var _a = duckElement.getBoundingClientRect(), left = _a.left, top = _a.top;
+    var outOfBoundX = duck.x < 0 || duck.x > window.innerWidth;
+    var outOfBoundY = duck.y < 0 || duck.y > window.innerHeight;
+    if (outOfBoundX) {
+        duck.speedX *= -1;
+    }
+    if (outOfBoundY) {
+        duck.speedY *= -1;
+    }
+    duck.x = left + duck.speedX;
+    duck.y = top + duck.speedY;
+    duckElement.style.left = duck.x + "px";
+    duckElement.style.top = duck.y + "px";
+    duckElement.style.backgroundImage = getDuckBackgroundImage(duckElement, duck);
+};
 var createDuck = function (count) {
-    return __spreadArray([], new Array(count), true).map(function () {
+    return __spreadArray([], __read(new Array(count)), false).map(function () {
         return {
             x: random(0, window.innerWidth),
             y: window.innerHeight,
@@ -31,8 +78,39 @@ var createDuck = function (count) {
     });
 };
 var execute = function () {
+    var e_1, _a;
     var ducks = createDuck(5);
     var duckElements = ducks.map(mapDuctToElement);
+    var _loop_1 = function (index, duckElement) {
+        var interval = setInterval(function () { return moveDuck(duckElement, ducks[index]); }, 100);
+        duckElement.addEventListener('click', function (event) {
+            var currentDuckElement = event.currentTarget;
+            currentDuckElement.style.transition = 'top 2s';
+            currentDuckElement.style.top = window.innerHeight + "px";
+            clearInterval(interval);
+            setTimeout(function () {
+                document.body.removeChild(currentDuckElement);
+                var duck = document.querySelector('.duck');
+                if (!duck) {
+                    var winningElem = document.querySelector('.winning');
+                    winningElem.style.opacity = '1';
+                }
+            }, 2000);
+        });
+    };
+    try {
+        for (var _b = __values(duckElements.entries()), _c = _b.next(); !_c.done; _c = _b.next()) {
+            var _d = __read(_c.value, 2), index = _d[0], duckElement = _d[1];
+            _loop_1(index, duckElement);
+        }
+    }
+    catch (e_1_1) { e_1 = { error: e_1_1 }; }
+    finally {
+        try {
+            if (_c && !_c.done && (_a = _b["return"])) _a.call(_b);
+        }
+        finally { if (e_1) throw e_1.error; }
+    }
 };
 execute();
 //# sourceMappingURL=start.js.map
--- src/06 - Duck Hunt/start.ts
+++ src/06 - Duck Hunt/start.ts
@@ -14,10 +14,35 @@ const mapDuctToElement = (duck:Duck) : HTMLDivElement => {
   duckElement.className = 'duck'
   duckElement.style.left = `${duck.x}px`
   duckElement.style.top = `${duck.y}px`
-  duckElement.style.background = `url(./left-1.png)`
   document.body.appendChild(duckElement);
 
   return duckElement
+}
+
+const getDuckBackgroundImage = (duckElement: HTMLDivElement, duck: Duck): string => {
+  const direction = duck.speedX > 0 ? 'right' : 'left'
+  const animetion = duckElement.style.backgroundImage.includes('1') ? '2' : '1'
+  return `url(./${direction}-${animetion}.png)`
+}
+
+const moveDuck = (duckElement: HTMLDivElement, duck: Duck): void => {
+  const {left, top} = duckElement.getBoundingClientRect()
+  const outOfBoundX = duck.x < 0 || duck.x > window.innerWidth
+  const outOfBoundY = duck.y < 0 || duck.y > window.innerHeight
+
+  if (outOfBoundX) {
+    duck.speedX *= -1
+  }
+
+  if (outOfBoundY) {
+    duck.speedY *= -1
+  }
+
+  duck.x = left + duck.speedX
+  duck.y = top + duck.speedY
+  duckElement.style.left = `${duck.x}px`;
+  duckElement.style.top = `${duck.y}px`;
+  duckElement.style.backgroundImage = getDuckBackgroundImage(duckElement, duck)
 }
 
 const createDuck = (count: number) : Duck[]=> {
@@ -34,7 +59,26 @@ const createDuck = (count: number) : Duck[]=> {
 const execute = () => {
   const ducks = createDuck(5)
   const duckElements = ducks.map(mapDuctToElement)
-  
+  for (const [index, duckElement] of duckElements.entries()) {
+    const interval = setInterval(() => moveDuck(duckElement, ducks[index]), 100)
+    duckElement.addEventListener('click', (event) => {
+      
+      const currentDuckElement = event.currentTarget as HTMLDivElement
+      currentDuckElement.style.transition = 'top 2s';
+      currentDuckElement.style.top = `${window.innerHeight}px`;
+
+      clearInterval(interval);
+      setTimeout(() => {
+        document.body.removeChild(currentDuckElement);
+
+        const duck = document.querySelector('.duck');
+        if (!duck) {
+          const winningElem = document.querySelector('.winning') as HTMLHeadElement
+          winningElem.style.opacity = '1';
+        }
+      }, 2000)
+    })
+  }
 }
 
 execute()
